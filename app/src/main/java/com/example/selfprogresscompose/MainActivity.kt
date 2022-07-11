fc5479bb0d55f01bf78b0f558a927dde8b4ab1c3
AnnaFVL <annsvist@gmail.com>
2022-07-11 15:29:34 +1000

    v.0.4 - ViewModel doesn't work

--- app/src/main/java/com/example/selfprogresscompose/MainActivity.kt
+++ app/src/main/java/com/example/selfprogresscompose/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.selfprogresscompose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -21,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
@@ -29,6 +32,10 @@ import com.example.selfprogresscompose.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // ViewModel
+        val model: MainActivityViewModel by viewModels()
+
 
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,30 +46,81 @@ class MainActivity : ComponentActivity() {
                 stringResource(id = R.string.sun))
             val activityList = listOf(stringResource(id = R.string.basic), stringResource(id = R.string.cardio),
                 stringResource(id = R.string.press), stringResource(id = R.string.stretching))
-            val basicCheckBoxList = remember {
-                listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
-                    mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
+
+            //val model: MainActivityViewModel by viewModels()
+            val selfProgressResult = model.getCalculatedResult()
+
+            // Get values from SharedPreferences
+            val context = LocalContext.current
+            model.getListsValues(context)
+
+            val uiState = model.uiState
+
+            // TODO - change to cycle?
+            /*val basicCheckBoxList = remember {
+                listOf(mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
                     mutableStateOf(false))
             }
             val cardioCheckBoxList = remember {
-                listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
-                    mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
+                listOf(mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
                     mutableStateOf(false))
             }
             val pressCheckBoxList = remember {
-                listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
-                    mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
+                listOf(mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
                     mutableStateOf(false))
             }
             val stretchingCheckBoxList = remember {
-                listOf(mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
-                    mutableStateOf(false), mutableStateOf(false), mutableStateOf(false),
+                listOf(mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
+                    mutableStateOf(false), mutableStateOf(false),
                     mutableStateOf(false))
+            }*/
+/*
+            val basicCheckBoxList = remember {
+                mutableListOf(mutableStateOf(model.getBasicList()[0]), mutableStateOf(model.getBasicList()[1]),
+                    mutableStateOf(model.getBasicList()[2]), mutableStateOf(model.getBasicList()[3]),
+                    mutableStateOf(model.getBasicList()[4]), mutableStateOf(model.getBasicList()[5]),
+                    mutableStateOf(model.getBasicList()[6]))
             }
+            /*val basicCheckBoxList = remember {
+                listOf(mutableStateOf(model.getBasicList()[0]), mutableStateOf(model.getBasicList()[1]),
+                    mutableStateOf(model.getBasicList()[2]), mutableStateOf(model.getBasicList()[3]),
+                    mutableStateOf(model.getBasicList()[4]), mutableStateOf(model.getBasicList()[5]),
+                    mutableStateOf(model.getBasicList()[6]))
+            }*/
+            val cardioCheckBoxList = remember {
+                mutableListOf(mutableStateOf(model.getCardioList()[0]), mutableStateOf(model.getCardioList()[1]),
+                    mutableStateOf(model.getCardioList()[2]), mutableStateOf(model.getCardioList()[3]),
+                    mutableStateOf(model.getCardioList()[4]), mutableStateOf(model.getCardioList()[5]),
+                    mutableStateOf(model.getCardioList()[6]))
+            }
+            val pressCheckBoxList = remember {
+                mutableListOf(mutableStateOf(model.getPressList()[0]), mutableStateOf(model.getPressList()[1]),
+                    mutableStateOf(model.getPressList()[2]), mutableStateOf(model.getPressList()[3]),
+                    mutableStateOf(model.getPressList()[4]), mutableStateOf(model.getPressList()[5]),
+                    mutableStateOf(model.getPressList()[6]))
+            }
+            val stretchingCheckBoxList = remember {
+                mutableListOf(
+                    mutableStateOf(model.getStretchingList()[0]),
+                    mutableStateOf(model.getStretchingList()[1]),
+                    mutableStateOf(model.getStretchingList()[2]),
+                    mutableStateOf(model.getStretchingList()[3]),
+                    mutableStateOf(model.getStretchingList()[4]),
+                    mutableStateOf(model.getStretchingList()[5]),
+                    mutableStateOf(model.getStretchingList()[6])
+                )
+            }*/
 
-            val model: MainActivityViewModel by viewModels()
 
-            val selfProgressResult = remember { mutableStateOf("Сейчас посчитаем...") }
+
+            //val selfProgressResult = remember { mutableStateOf("Сейчас посчитаем...") }
             //val basic1CheckBox = remember { mutableStateOf(false) }
 
             //////
@@ -148,7 +206,23 @@ class MainActivity : ComponentActivity() {
                                 .weight(1f),
                             verticalArrangement = Arrangement.Top
                         ) {
-                            var currentCheckBoxList = listOf<MutableState<Boolean>>()
+
+                            for (weekDayNumber in weekDayList.indices) {
+                                Checkbox(checked = uiState.tmpList[weekDayNumber],
+                                    onCheckedChange = {
+                                        uiState.tmpList[weekDayNumber] = it
+                                        when (activityItem) {
+                                            getString(R.string.basic) -> model.getBasicList()[weekDayNumber] = it
+                                            getString(R.string.cardio) -> model.getCardioList()[weekDayNumber] = it
+                                            getString(R.string.press) -> model.getPressList()[weekDayNumber] = it
+                                            getString(R.string.stretching) -> model.getStretchingList()[weekDayNumber] = it
+                                        }
+
+                                    },
+                                    colors  = CheckboxDefaults.colors(checkedColor = checkBoxColor, checkmarkColor = checkBoxSubColor))
+                            }
+
+                        /*var currentCheckBoxList = mutableListOf<MutableState<Boolean>>()
                             when (activityItem) {
                                 stringResource(id = R.string.basic) -> currentCheckBoxList = basicCheckBoxList
                                 stringResource(id = R.string.cardio) -> currentCheckBoxList = cardioCheckBoxList
@@ -159,9 +233,16 @@ class MainActivity : ComponentActivity() {
                                 Checkbox(checked = currentCheckBoxList[weekDayNumber].value,
                                     onCheckedChange = {
                                         currentCheckBoxList[weekDayNumber].value = it
+                                        when (activityItem) {
+                                            getString(R.string.basic) -> model.getBasicList()[weekDayNumber] = it
+                                            getString(R.string.cardio) -> model.getCardioList()[weekDayNumber] = it
+                                            getString(R.string.press) -> model.getPressList()[weekDayNumber] = it
+                                            getString(R.string.stretching) -> model.getStretchingList()[weekDayNumber] = it
+                                        }
+
                                     },
                                     colors  = CheckboxDefaults.colors(checkedColor = checkBoxColor, checkmarkColor = checkBoxSubColor))
-                            }
+                            }*/
                             /*
                             for (weekDayNumber in weekDayList.indices) {
                                 Checkbox(checked = basicCheckBoxList[weekDayNumber].value,
@@ -180,16 +261,21 @@ class MainActivity : ComponentActivity() {
             // Кнопка
             //////
             Button(
-                onClick = {
-                          val random = java.util.Random().nextInt(5)
+                onClick = { caclResult(model, context) },/*{
+
+                          //val random = java.util.Random().nextInt(5)
+                    /*
                     when (random) {
                         0 -> selfProgressResult.value = "Надо больше стараться!"
                         1 -> selfProgressResult.value = "Неплохо! Может не все удалось, но ты старалась"
                         2 -> selfProgressResult.value = "Статус: герой ;)"
                         3 -> selfProgressResult.value = "Кто будет лениться, тот конфетки не ест ;)"
                         4 -> selfProgressResult.value = "Отлично! Все получилось!"
-                    }
-                },
+                    }*/
+
+                    // Save values in SharedPreferences
+                    //model.saveListsValues(context)
+                },*/
                 colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor, contentColor = Color.White),
                 modifier = Modifier
                     .padding(start = borderPadding, top = 400.dp, end = borderPadding)
@@ -204,7 +290,7 @@ class MainActivity : ComponentActivity() {
                 fontSize = captionTextSize,
                 color = basicTextColor,
                 modifier = Modifier.padding(start=borderPadding, top=450.dp))
-            Text(text=model.getCalculatedResult(), //selfProgressResult.value, // model.getCalculatedResult()
+            Text(text=selfProgressResult, // model.getCalculatedResult()
                 fontSize = headerTextSize,
                 color = goodTextColor,
                 modifier = Modifier.padding(start=borderPadding, top=470.dp))
@@ -216,6 +302,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+
+fun caclResult(model: MainActivityViewModel, context: Context) {
+    model.saveListsValues(context)
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/selfprogresscompose/MainActivityViewModel.kt
+++ app/src/main/java/com/example/selfprogresscompose/MainActivityViewModel.kt
@@ -1,11 +1,87 @@
 package com.example.selfprogresscompose
 
+import android.content.Context
+import android.content.res.Resources
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+data class UiState(
+    val tmpList : MutableList<Boolean>  = mutableListOf (false, false, false, false, false, false, false)
+)
 
 class MainActivityViewModel: ViewModel() {
-    private val resultText = "Text text from View Model"
+    private val sharedPreferencesFileName = "selfProgressAppSharedPreferences"
+
+    private val resultText = "Test" //Resources.getSystem().getString(R.string.result_text_unknown)
+
+    private val basicList = mutableListOf (false, false, false, false, false, false, false)
+    private val cardioList = mutableListOf (false, false, false, false, false, false, false)
+    private val pressList = mutableListOf (false, false, false, false, false, false, false)
+    private val stretchingList = mutableListOf (false, false, false, false, false, false, false)
+
+
+
+    var uiState by mutableStateOf(UiState())
+        private set
+
 
     fun getCalculatedResult(): String {
         return resultText
+    }
+
+    fun getBasicList(): MutableList<Boolean> {
+        return basicList
+    }
+
+    fun getCardioList(): MutableList<Boolean> {
+        return cardioList
+    }
+
+    fun getPressList(): MutableList<Boolean> {
+        return pressList
+    }
+
+    fun getStretchingList(): MutableList<Boolean> {
+        return stretchingList
+    }
+
+    fun getListsValues(context: Context) {
+        val sharedPreferences = context.getSharedPreferences(sharedPreferencesFileName, Context.MODE_PRIVATE)
+        /*for (index in basicList.indices) {
+            if (boolList[index].value)
+            basicList[index] = sharedPreferences.getBoolean("basic$index", false)
+        }
+        for (index in cardioList.indices) {
+            cardioList[index] = sharedPreferences.getBoolean("cardio$index", false)
+        }
+        for (index in pressList.indices) {
+            pressList[index] = sharedPreferences.getBoolean("press$index", false)
+        }
+        for (index in stretchingList.indices) {
+            stretchingList[index] = sharedPreferences.getBoolean("stretching$index", false)
+        }*/
+
+    }
+
+    fun saveListsValues(context: Context) {
+        val sharedPreferences = context.getSharedPreferences(sharedPreferencesFileName, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        /*for (index in basicList.indices) {
+            editor.putBoolean("basic$index", basicList[index])
+        }
+        for (index in cardioList.indices) {
+            editor.putBoolean("cardio$index", cardioList[index])
+        }
+        for (index in pressList.indices) {
+            editor.putBoolean("press$index", pressList[index])
+        }
+        for (index in stretchingList.indices) {
+            editor.putBoolean("stretching$index", stretchingList[index])
+        }*/
+        editor.apply()
     }
 }
